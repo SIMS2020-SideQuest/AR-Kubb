03df3f1e9b6661a1bb6ce0730f98e5f920e0751e
mamu1700 <60223524+mamu1700@users.noreply.github.com>
2020-09-17 11:26:15 +0200

    Fixat så att man navigeras till spelet när play trycks

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public void playGame() //funktion för playbutton
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Plussar på 1 så att nästa scene loadas 
+    }
+}
