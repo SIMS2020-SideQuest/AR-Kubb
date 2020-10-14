0b2c48f9012b3173e03dc9b3e079b849ddef036f
keppoloco <70692551+keppoloco@users.noreply.github.com>
2020-10-14 13:42:38 +0200

    Added Scene & Script

---
+++ Assets/Scripts/objectCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectCollision : MonoBehaviour
+{
+    Rigidbody obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GetComponent<Rigidbody>();
+
+        obj.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collosion)
+    {
+        
+    }
+}
